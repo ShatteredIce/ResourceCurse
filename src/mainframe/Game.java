86d562a2afa1056dacbb28bb944739ba9c3cf80b
ShatteredIce <icywind.katsuo@gmail.com>
2019-01-05 13:52:47 -0800

    added game engine class to store lwjgl3 initialization code

--- src/mainframe/Game.java
+++ src/mainframe/Game.java
@@ -16,145 +16,32 @@ import static org.lwjgl.system.MemoryUtil.*;
 
 public class Game {
 
-	// The window handle
-	private long window;
 	//Rendering variables
 	final double[] textureCoords = {0, 0, 0, 1, 1, 0, 1, 1};
 	final int[] indices = {0, 1, 2, 2, 1, 3};
 	final double[] placeholder = {0, 0, 0, 0, 0, 0, 0, 0};
 	Model model;
 	
-	//Setup for variables for game frame
-	int WINDOW_WIDTH = 840;
-	int WINDOW_HEIGHT = 640;
-	
-	int gameScreenWidth = 640;
-	int gameScreenHeight = 640;
-	
-	//Setup variables for camera
-	public double viewX = 0;
-	public double viewY = 0;
-	public double cameraSpeed = 10;
-	public double cameraWidth = 840;
-	public double cameraHeight = 640;
-	
-	public int windowXOffset = 0;
-	public int windowYOffset = 0;
+	GameEngine lwjgl3 = new GameEngine(this);
 	
 	Map gamemap;
 
 	public void run() {
-
-		initGLFW();
-		setup();
-
-		// Free the window callbacks and destroy the window
-		glfwFreeCallbacks(window);
-		glfwDestroyWindow(window);
-
-		// Terminate GLFW and free the error callback
-		glfwTerminate();
-		glfwSetErrorCallback(null).free();
+		lwjgl3.create();
+		start();
+		lwjgl3.destroy();
 	}
 
-	private void initGLFW() {
-		// Setup an error callback. The default implementation
-		// will print the error message in System.err.
-		GLFWErrorCallback.createPrint(System.err).set();
-
-		// Initialize GLFW. Most GLFW functions will not work before doing this.
-		if ( !glfwInit() )
-			throw new IllegalStateException("Unable to initialize GLFW");
-
-		// Configure GLFW
-		glfwDefaultWindowHints(); // optional, the current window hints are already the default
-		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
-		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
-
-		// Create the window
-		window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "ResourceCurse [Development]", NULL, NULL);
-		if ( window == NULL )
-			throw new RuntimeException("Failed to create the GLFW window");
-
-		// Setup a key callback. It will be called every time a key is pressed, repeated or released.
-		glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-			if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
-				glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
-		});
+	private void start() {
 		
-		//mouse clicks
-		glfwSetMouseButtonCallback (window, (window, button, action, mods) -> {
-			DoubleBuffer xpos = BufferUtils.createDoubleBuffer(3);
-			DoubleBuffer ypos = BufferUtils.createDoubleBuffer(3);
-			glfwGetCursorPos(window, xpos, ypos);
-			//convert the glfw coordinate to our coordinate system
-			xpos.put(0, Math.min(Math.max(xpos.get(0), windowXOffset), WINDOW_WIDTH + windowXOffset));
-			ypos.put(0, Math.min(Math.max(ypos.get(0), windowYOffset), WINDOW_HEIGHT + windowYOffset));
-			//relative camera coordinates
-			xpos.put(1, getWidthScalar() * (xpos.get(0) - windowXOffset) + viewX);
-			ypos.put(1, getHeightScalar() * (ypos.get(0) - windowYOffset) + viewY);
-			//true window coordinates
-			xpos.put(2, xpos.get(0) - windowXOffset);
-			ypos.put(2, ypos.get(0) - windowYOffset);
-			if ( button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
-				System.out.println("Left Mouse Button: " + xpos.get(0) + " " + ypos.get(0));
-			}
-			else if( button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS) {
-				System.out.println("Right Mouse Button: " + xpos.get(0) + " " + ypos.get(0));
-			}
-		});
-
-		// Get the thread stack and push a new frame
-		try ( MemoryStack stack = stackPush() ) {
-			IntBuffer pWidth = stack.mallocInt(1); // int*
-			IntBuffer pHeight = stack.mallocInt(1); // int*
-
-			// Get the window size passed to glfwCreateWindow
-			glfwGetWindowSize(window, pWidth, pHeight);
-
-			// Get the resolution of the primary monitor
-			GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-
-			// Center the window
-			glfwSetWindowPos(
-				window,
-				(vidmode.width() - pWidth.get(0)) / 2,
-				(vidmode.height() - pHeight.get(0)) / 2
-			);
-		} // the stack frame is popped automatically
-
-		// Make the OpenGL context current
-		glfwMakeContextCurrent(window);
-		// Enable v-sync
-		glfwSwapInterval(1);
-
-		// Make the window visible
-		glfwShowWindow(window);
-	}
-
-	private void setup() {
-		// This line is critical for LWJGL's interoperation with GLFW's
-		// OpenGL context, or any context that is managed externally.
-		// LWJGL detects the context that is current in the current thread,
-		// creates the GLCapabilities instance and makes the OpenGL
-		// bindings available for use.
-		GL.createCapabilities();
-
-		// Set the clear color
-		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
-		
-		//Enable transparency
-		glEnable(GL_BLEND);
-		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-		
-		glEnable(GL_TEXTURE_2D);
+		lwjgl3.setup();
 		
 		model = new Model(placeholder, textureCoords, indices);
 		gamemap = new TestMap();
 
 		// Run the rendering loop until the user has attempted to close
 		// the window or has pressed the ESCAPE key.
-		while ( !glfwWindowShouldClose(window) ) {			
+		while ( !glfwWindowShouldClose(lwjgl3.getWindowHandle()) ) {			
 			// Poll for window events. The key callback above will only be
 			// invoked during this call.
 			glfwPollEvents();
@@ -163,51 +50,27 @@ public class Game {
 
 			gameLoop();
 
-			glfwSwapBuffers(window); // swap the color buffers
+			glfwSwapBuffers(lwjgl3.getWindowHandle()); // swap the color buffers
 		}
 	}
 	
 	public void gameLoop() {
-		projectRelativeCameraCoordinates();
+		lwjgl3.projectRelativeCameraCoordinates();
 		gamemap.getTexture().bind();
-		model.render(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+		model.render(0, 0, lwjgl3.getWindowWidth(), lwjgl3.getWindowHeight());
+	}
+	
+	public void onMouseClick(int button, int action, DoubleBuffer xpos, DoubleBuffer ypos) {
+		if ( button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+			System.out.println("Left Mouse Button: " + xpos.get(0) + " " + ypos.get(0));
+		}
+		else if( button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS) {
+			System.out.println("Right Mouse Button: " + xpos.get(0) + " " + ypos.get(0));
+		}
 	}
 
 	public static void main(String[] args) {
 		new Game().run();
-	}
-	
-	//Screen projection based on relative camera coordinates
-	public void projectRelativeCameraCoordinates(){
-		glMatrixMode(GL_PROJECTION);
-        glLoadIdentity(); // Resets any previous projection matrices
-        glOrtho((-windowXOffset * getWidthScalar()) + viewX, viewX + cameraWidth + (windowXOffset * getWidthScalar()), viewY + cameraHeight + ((windowYOffset)* getHeightScalar()), viewY + ((-windowYOffset) * getHeightScalar()), 1, -1);
-        glMatrixMode(GL_MODELVIEW);
-	}
-		
-	//Screen projection based on true window coordinates
-	public void projectTrueWindowCoordinates(){
-		glMatrixMode(GL_PROJECTION);
-        glLoadIdentity(); // Resets any previous projection matrices
-        glOrtho(-windowXOffset, WINDOW_WIDTH + windowXOffset, WINDOW_HEIGHT + windowYOffset, -windowYOffset, 1, -1);
-        glMatrixMode(GL_MODELVIEW);
-	}
-		
-	//Scalars to help calculation
-	public double getWidthScalar(){
-		return(double) cameraWidth / (double) WINDOW_WIDTH;
-	}
-		
-	public double getHeightScalar(){
-		return(double) cameraHeight / (double) WINDOW_HEIGHT;
-	}
-	
-	public double mapWidthScalar() {
-		return (double) gameScreenWidth / (double) WINDOW_WIDTH;
-	}
-	
-	public double mapHeightScalar() {
-		return (double) gameScreenHeight / (double) WINDOW_HEIGHT;
 	}
 
 }
---
+++ src/mainframe/GameEngine.java
@@ -0,0 +1,222 @@
+package mainframe;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.GLFW_FALSE;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
+import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
+import static org.lwjgl.glfw.GLFW.GLFW_TRUE;
+import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
+import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
+import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
+import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
+import static org.lwjgl.glfw.GLFW.glfwGetCursorPos;
+import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
+import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
+import static org.lwjgl.glfw.GLFW.glfwGetWindowSize;
+import static org.lwjgl.glfw.GLFW.glfwInit;
+import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
+import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetKeyCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetMouseButtonCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowShouldClose;
+import static org.lwjgl.glfw.GLFW.glfwShowWindow;
+import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
+import static org.lwjgl.glfw.GLFW.glfwTerminate;
+import static org.lwjgl.glfw.GLFW.glfwWindowHint;
+import static org.lwjgl.opengl.GL11.GL_BLEND;
+import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
+import static org.lwjgl.opengl.GL11.GL_ONE_MINUS_SRC_ALPHA;
+import static org.lwjgl.opengl.GL11.GL_PROJECTION;
+import static org.lwjgl.opengl.GL11.GL_SRC_ALPHA;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
+import static org.lwjgl.opengl.GL11.glBlendFunc;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL11.glEnable;
+import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glMatrixMode;
+import static org.lwjgl.opengl.GL11.glOrtho;
+import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+import java.nio.DoubleBuffer;
+import java.nio.IntBuffer;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.system.MemoryStack;
+
+public class GameEngine {
+	
+	static Game game;
+	
+	// The window handle
+	private long window;
+	
+	final int WINDOW_WIDTH = 840;
+	final int WINDOW_HEIGHT = 640;
+	
+	//Setup for variables for game frame
+	
+	int gameScreenWidth = 640;
+	int gameScreenHeight = 640;
+	
+	//Setup variables for camera
+	public double viewX = 0;
+	public double viewY = 0;
+	public double cameraSpeed = 10;
+	public double cameraWidth = 840;
+	public double cameraHeight = 640;
+	
+	public int windowXOffset = 0;
+	public int windowYOffset = 0;
+
+	public GameEngine(Game newgame) {
+		game = newgame;
+	}
+	
+	public void create() {
+		// Setup an error callback. The default implementation
+		// will print the error message in System.err.
+		GLFWErrorCallback.createPrint(System.err).set();
+
+		// Initialize GLFW. Most GLFW functions will not work before doing this.
+		if ( !glfwInit() )
+			throw new IllegalStateException("Unable to initialize GLFW");
+
+		// Configure GLFW
+		glfwDefaultWindowHints(); // optional, the current window hints are already the default
+		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+		// Create the window
+		window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "ResourceCurse [Development]", NULL, NULL);
+		if ( window == NULL ) {
+			throw new RuntimeException("Failed to create the GLFW window");
+		}
+		// Setup a key callback. It will be called every time a key is pressed, repeated or released.
+		glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+			if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+				glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
+		});
+		
+		//mouse clicks
+		glfwSetMouseButtonCallback (window, (window, button, action, mods) -> {
+			DoubleBuffer xpos = BufferUtils.createDoubleBuffer(3);
+			DoubleBuffer ypos = BufferUtils.createDoubleBuffer(3);
+			glfwGetCursorPos(window, xpos, ypos);
+			//convert the glfw coordinate to our coordinate system
+			xpos.put(0, Math.min(Math.max(xpos.get(0), windowXOffset), WINDOW_WIDTH + windowXOffset));
+			ypos.put(0, Math.min(Math.max(ypos.get(0), windowYOffset), WINDOW_HEIGHT + windowYOffset));
+			//relative camera coordinates
+			xpos.put(1, getWidthScalar() * (xpos.get(0) - windowXOffset) + viewX);
+			ypos.put(1, getHeightScalar() * (ypos.get(0) - windowYOffset) + viewY);
+			//true window coordinates
+			xpos.put(2, xpos.get(0) - windowXOffset);
+			ypos.put(2, ypos.get(0) - windowYOffset);
+			game.onMouseClick(button, action, xpos, ypos);
+		});
+
+		// Get the thread stack and push a new frame
+		try ( MemoryStack stack = stackPush() ) {
+			IntBuffer pWidth = stack.mallocInt(1); // int*
+			IntBuffer pHeight = stack.mallocInt(1); // int*
+
+			// Get the window size passed to glfwCreateWindow
+			glfwGetWindowSize(window, pWidth, pHeight);
+
+			// Get the resolution of the primary monitor
+			GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+			// Center the window
+			glfwSetWindowPos(
+				window,
+				(vidmode.width() - pWidth.get(0)) / 2,
+				(vidmode.height() - pHeight.get(0)) / 2
+			);
+		} // the stack frame is popped automatically
+
+		// Make the OpenGL context current
+		glfwMakeContextCurrent(window);
+		// Enable v-sync
+		glfwSwapInterval(1);
+
+		// Make the window visible
+		glfwShowWindow(window);
+	}
+	
+	public void setup() {
+		// This line is critical for LWJGL's interoperation with GLFW's
+		// OpenGL context, or any context that is managed externally.
+		// LWJGL detects the context that is current in the current thread,
+		// creates the GLCapabilities instance and makes the OpenGL
+		// bindings available for use.
+		GL.createCapabilities();
+
+		// Set the clear color
+		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+		
+		//Enable transparency
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		
+		glEnable(GL_TEXTURE_2D);
+	}
+	
+	public void destroy() {
+		// Free the window callbacks and destroy the window
+		glfwFreeCallbacks(window);
+		glfwDestroyWindow(window);
+		
+		// Terminate GLFW and free the error callback
+		glfwTerminate();
+		glfwSetErrorCallback(null).free();
+	}
+	
+	public long getWindowHandle() {
+		return window;
+	}
+	
+	//Screen projection based on relative camera coordinates
+		public void projectRelativeCameraCoordinates(){
+			glMatrixMode(GL_PROJECTION);
+	        glLoadIdentity(); // Resets any previous projection matrices
+	        glOrtho((-windowXOffset * getWidthScalar()) + viewX, viewX + cameraWidth + (windowXOffset * getWidthScalar()), viewY + cameraHeight + ((windowYOffset)* getHeightScalar()), viewY + ((-windowYOffset) * getHeightScalar()), 1, -1);
+	        glMatrixMode(GL_MODELVIEW);
+		}
+			
+		//Screen projection based on true window coordinates
+		public void projectTrueWindowCoordinates(){
+			glMatrixMode(GL_PROJECTION);
+	        glLoadIdentity(); // Resets any previous projection matrices
+	        glOrtho(-windowXOffset, WINDOW_WIDTH + windowXOffset, WINDOW_HEIGHT + windowYOffset, -windowYOffset, 1, -1);
+	        glMatrixMode(GL_MODELVIEW);
+		}
+			
+		//Scalars to help calculation
+		public double getWidthScalar(){
+			return(double) cameraWidth / (double) WINDOW_WIDTH;
+		}
+			
+		public double getHeightScalar(){
+			return(double) cameraHeight / (double) WINDOW_HEIGHT;
+		}
+		
+		public double mapWidthScalar() {
+			return (double) gameScreenWidth / (double) WINDOW_WIDTH;
+		}
+		
+		public double mapHeightScalar() {
+			return (double) gameScreenHeight / (double) WINDOW_HEIGHT;
+		}
+		
+		public int getWindowWidth() {
+			return WINDOW_WIDTH;
+		}
+		public int getWindowHeight() {
+			return WINDOW_HEIGHT;
+		}
+	
+}
